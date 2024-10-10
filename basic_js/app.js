d5b1c8b5f6415dda501ace01bc25979f243af7cf
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-10-10 21:47:43 +0600

    Day Two Part_02

--- basic_js/app.js
+++ basic_js/app.js
@@ -1,3 +1,27 @@
 
 
+
+// Declare variable in JS
+
+var name = 'Arno';
+name = 'Mahmudul'
+console.log(name);
+
+let age = 20;
+console.log(age);
+
+const country = 'Bangladesh';
+console.log(country);
+
+
+
+/*
+// Finish
+// Date 10.10.2024
 alert('Hi, This is Mamun.');
+// Method Based on Console
+console.log('Hello World!!!');
+console.error('Error inbound!');
+console.warn('This is Your Final Warning!');
+*/
+
