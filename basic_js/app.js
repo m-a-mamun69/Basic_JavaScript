a6267817a70adcace0289fa4ece2bc6037d7d20e
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-10-11 17:56:42 +0600

    Day Three Part_01.

--- basic_js/app.js
+++ basic_js/app.js
@@ -1,5 +1,42 @@
 
 
+// Data Type in JS
+
+let length = 16;
+let weight = 7.5;
+console.log(length);
+console.log(weight);
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color);
+console.log(lastName);
+
+// Booleans
+let x = true;
+let y = false;
+console.log(x);
+console.log(y);
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person);
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+// Date object:
+const date = new Date("2022-03-25");
+
+console.log(date);
+
+
+/*
+
+// Finish
+// Date 10.10.2024
 
 // Declare variable in JS
 
@@ -13,9 +50,10 @@ console.log(age);
 const country = 'Bangladesh';
 console.log(country);
 
-
+*/
 
 /*
+
 // Finish
 // Date 10.10.2024
 alert('Hi, This is Mamun.');
@@ -23,5 +61,6 @@ alert('Hi, This is Mamun.');
 console.log('Hello World!!!');
 console.error('Error inbound!');
 console.warn('This is Your Final Warning!');
+
 */
 
