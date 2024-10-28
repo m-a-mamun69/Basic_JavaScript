68fc2353853639c7d280bb62299e3a73c1813156
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-10-28 17:32:30 +0600

    Basic JS With Bro..

---
+++ basic_js_bro/index.js
@@ -0,0 +1,21 @@
+console.log(`Hello`);
+console.log(`I Like Pizza!`);
+
+window.alert('This is an Alart.')
+window.alert('I Like Pizza.')
+
+document.getElementById('myH1').textContent = 'Hello, World!,'
+document.getElementById('myP1').textContent = 'This is M A Mamun,'
+
+
+
+
+// For Comment Single Line 
+
+/*
+    There is A 
+    Multiline 
+    Comment
+*/
+
+
