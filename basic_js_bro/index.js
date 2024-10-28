783cbe6ff09f790f442956f5f9de5e3ba23d73cf
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-10-28 22:35:18 +0600

    Basic JS With Bro..( Variables )

--- basic_js_bro/index.js
+++ basic_js_bro/index.js
@@ -1,13 +1,61 @@
-console.log(`Hello`);
-console.log(`I Like Pizza!`);
+// console.log(`Hello`);
+// console.log(`I Like Pizza!`);
 
-window.alert('This is an Alart.')
-window.alert('I Like Pizza.')
+// window.alert('This is an Alart.')
+// window.alert('I Like Pizza.')
 
-document.getElementById('myH1').textContent = 'Hello, World!,'
-document.getElementById('myP1').textContent = 'This is M A Mamun,'
+// document.getElementById('myH1').textContent = 'Hello, World!,'
+// document.getElementById('myP1').textContent = 'This is M A Mamun,'
+
+/*
+// ------ Number -------------- Date: 28.10.24
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.80;
+
+console.log(typeof(gpa))
+console.log(`Your are a ${age} Year Old.`);
+console.log(`T-Shirt Price is ${price}`);
+console.log(`Your GPA is ${gpa}`);
+*/
 
 
+/*
+// ------ String -------------- Same
+
+let firstName = "Al-Mamun";
+let favColor = "Gray";
+let email = "mamun69@gmail.com";
+
+console.log(typeof(email))
+console.log(`Your name is ${firstName} Year Old.`);
+console.log(`You Like  ${favColor}`);
+console.log(`Your Email is ${email}`);
+*/
+
+
+/*
+// ------ Booleans -------------- Same
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(typeof(forSale))
+console.log(`Are You Online? ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+*/
+
+
+
+let fullName = "Al-Mamun";
+let age = 25;
+let isStudent = false;
+document.getElementById('myP1').textContent = `Your name is: ${fullName}`;
+document.getElementById('myP2').textContent = `Your are a ${age} Year Old.`;
+document.getElementById('myP3').textContent = `Enrolled: ${isStudent}`;
 
 
 // For Comment Single Line 
