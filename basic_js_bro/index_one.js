fefc0c5de4ca4fbe4374d84637a99ac66b7f4887
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-11-04 13:30:45 +0600

    JS STRING SLICING Stuff.

--- basic_js_bro/index_one.js
+++ basic_js_bro/index_one.js
@@ -169,7 +169,7 @@ console.log(`The Letter Grade is: ${letterGrade}`);
 
 // --------------- TERNARY OPERATOR ---------------- !
 
-
+/*
 let userName = "Nasurulla";
 let output;
 // let result = userName.startsWith(" ")
@@ -193,3 +193,25 @@ else{
 }
 
 // console.log(output);
+*/
+
+
+// --------------- JS SWITCHES  -------------- Date: 4.11.24
+
+
+// const fullName = 'mCode mamun';
+const email = 'Mamun@gmail.com';
+
+// let firstName = fullName.slice(0, 2);
+// let lastName = fullName.slice(4, 9);
+
+// let firstChar = fullName.slice(0, 1);
+// let lastChar = fullName.slice(-1);
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+let userName = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(userName);
+console.log(extension);
