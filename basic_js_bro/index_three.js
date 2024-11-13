357d6ee47a40d8fef4413f2bca55bb4c48949c4c
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-11-13 16:18:39 +0600

    Doing JS map() Method Stuff.

--- basic_js_bro/index_three.js
+++ basic_js_bro/index_three.js
@@ -120,7 +120,7 @@ function displayPage(result){
 */
 
 
-// ----------------- JS CallBack Function ----------------- !
+// ----------------- JS forEach() Method ----------------- !
 
 // Explanation
 /*
@@ -158,7 +158,7 @@ function display(element, index, array){
     console.log(element);
 }
 */
-
+/*
 let fruits = ["apple", "orange", "banana", "coconu"];
 
 fruits.forEach(capitalize);
@@ -178,4 +178,56 @@ function capitalize(element, index, array){
 
 function display(element){
     console.log(element);
+}
+*/
+
+
+// -------------- JS Random Password Generator -------------- Date: 12.11.24
+
+// Explanation
+/*
+ .map() = accepts a callback and applies that function 
+                 to each element of an array, then return a new array
+*/
+
+/*
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+console.log(cubes);
+
+function square(element){
+    return Math.pow(element, 2);
+}
+
+function cube(element){
+    return Math.pow(element, 3);
+}
+*/
+
+/*
+const students = ["Hasan", "Samim", "Harun", "Miltion"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(lowerCase);
+
+console.log(studentsLower);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+*/
+
+
+const dates = ["2024-1-10","2025-2-20","2026-3-30"];
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element){
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}`;
 }
