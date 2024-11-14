eec5217c290310aa51a4740d73ba86e551bf02e5
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-11-14 13:25:22 +0600

    JS .reduce() Method Stuff.

--- basic_js_bro/index_three.js
+++ basic_js_bro/index_three.js
@@ -276,7 +276,7 @@ function isChild(element){
 }
 */
 
-
+/*
 const words = ["apple", "orange", "banana", "dragon", "mango", "watermelon"];
 
 const shortWords = words.filter(getShortWords);
@@ -291,4 +291,40 @@ function getShortWords(element){
 
 function getLongWords(element){
     return element.length > 5;
+}
+*/
+
+
+// -------------- JS map() Method -------------- Date: 14.11.24
+
+// Explanation
+/*
+.reduce() = reduce the elements of an array
+                    to a single value
+*/ 
+
+/*
+const prices = [5, 30, 10, 25, 15, 20];
+const total = prices.reduce(sum);
+
+console.log(`The Sum is: $${total}`);
+
+function sum(accumulator, element){
+    return accumulator + element;
+}
+*/
+
+const grades = [75, 50, 90, 80, 65, 95];
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(`The Maximum is: $${maximum}`);
+console.log(`The Minimum is: $${minimum}`);
+
+function getMax(accumulator, element){
+    return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element){
+    return Math.min(accumulator, element);
 }
