d7b2137b6e4b7edf03c4156274b3f8c7064285e8
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-11-20 17:42:29 +0600

    JS NESTED OBJECTS Stuff.

--- basic_js_bro/index_five.js
+++ basic_js_bro/index_five.js
@@ -62,9 +62,8 @@ console.log(age);
 console.log(job);
 */
 
-
 // DESTRUCTURING in Function Parameter
-
+/*
 function displayPerson({firstName, lastName, age, job="Unemployed"}){
     console.log(`Name: ${firstName} ${lastName}`);
     console.log(`Age: ${age}`);
@@ -86,3 +85,73 @@ const person2 = {
 
 // displayPerson(person1);
 displayPerson(person2);
+*/
+
+
+// ------------------- JS NESTED OBJECTS ------------------- !
+
+// Explanation
+/*
+nested objects = Objects inside of other Objects.
+                              Allows you to represent more complex data structures
+                              Child Object is enclosed by a Parent Object
+
+                              Person{Address{}, ContactInfo{}}
+                              ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+*/
+
+/*
+// Example- 01
+const person = {
+    fullName: "Mamun Rashid",
+    age: 30,
+    isStudent: true,
+    hobbies: ["Coding","Reading","cooking"],
+    address:{
+        street: "Block-C, Mirpur-1",
+        city: "Dhaka",
+        country:"Bangladesh"
+    }
+}
+
+console.log(person.fullName);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.hobbies[1]);
+// console.log(person.address.city);
+
+for(const property in person.address){
+    console.log(person.address[property]);
+}
+*/
+
+
+class Person{
+    constructor(name, age, ...address){
+        this.name = name;
+        this.age = age;
+        this.address = new Address(...address);
+    }
+}
+
+class Address{
+    constructor(street, city, country){
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+const person1 = new Person("Mamun Rashid", 26, "Block-C, Mirpur-1",
+                                                "Dhaka",
+                                                "Bangladesh");
+
+const person2 = new Person("Hasan Rony", 27, "Saheeb Bazar",
+                                                    "Rajshahi",
+                                                    "Bangladesh");
+
+const person3 = new Person("Samim Reza", 27, "Sapahar",
+                                            "Naogaon",
+                                            "Bangladesh");
+console.log(person2.name);
+console.log(person2.address.city);
