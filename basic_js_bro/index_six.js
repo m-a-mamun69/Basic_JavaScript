901e019eae8f2ac8bd183894d6f8128d0c327bbe
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-11-30 17:26:02 +0600

    JS DOM Explained Stuff.

--- basic_js_bro/index_six.js
+++ basic_js_bro/index_six.js
@@ -214,3 +214,29 @@ catch(error){
 
 console.log("You have reached the end!");
 */
+
+// ---------------- JS DOM Explained ---------------- Date: 30.11.2024
+// Explanation
+/*
+DOM = DOCUMENT OBJECT MODEL
+             Object{} that represents the page you see in the web browser 
+             and provides you with an API to interact with it.
+            Web browser constructs the DOM when it loads an HTML document,
+            and structures all the elements in a tree-like representation.
+           JavaScript can access the DOM to dynamically 
+           change the content, structure, and style of a web page.
+*/
+
+/*
+document.title = "My Website";
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)";
+
+console.dir(document);
+*/
+
+const userName = "rashidCode";
+const welcomeMsg = document.getElementById("welcome-msg");
+
+welcomeMsg.textContent += userName === "" ? `Guest` : userName;
+
+
