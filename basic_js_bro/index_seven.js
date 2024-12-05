b3c03a7da119ec569725d45a5b97347ff286a926
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-12-05 21:38:37 +0600

    JS CALLBACK HELL.

--- basic_js_bro/index_seven.js
+++ basic_js_bro/index_seven.js
@@ -104,7 +104,7 @@ myButton.addEventListener("click", event => {
     }
 });
 */
-
+/*
 let buttons = document.querySelectorAll(".myButton");
 
 buttons.forEach(button =>  {
@@ -132,4 +132,52 @@ buttons.forEach(button =>  {
             event.target.classList.replace("enabled", "disabled");
         }
     });
+});
+*/
+
+
+// ---------------- JS CALLBACK HELL ---------------- Date: 05.12.2024
+// Explanation
+/*
+Callback Hell = Situation in JavaScript where callbacks 
+                           are nested within other callbacks to the
+                           degree where the code is difficult to read.
+                           Old pattern to handle asynchronous functions.
+                           Use Promises + async/await to avoid Callback Hell
+*/
+
+function task1(callback){
+    setTimeout(() => {
+        console.log("Task 1 Complete.");
+        callback();
+    }, 2000);
+}
+
+function task2(callback){
+    setTimeout(() => {
+        console.log("Task 2 Complete.");
+        callback();
+    }, 1000);
+}
+
+function task3(callback){
+    setTimeout(() => {
+        console.log("Task 3 Complete.");
+        callback();
+    }, 3000);
+}
+
+function task4(callback){
+    setTimeout(() => {
+        console.log("Task 4 Complete.");
+        callback();
+    }, 1500);
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {console.log("All Tasks Complete.");})
+        })
+    })
 });
